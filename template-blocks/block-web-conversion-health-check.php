4f0d9a24dd19360eef75909a52f9c9d4c567a884
Chris Chong <chris.chong@firstpage.com.au>
2021-11-30 17:40:39 +1100

    scss updates

--- template-blocks/block-web-conversion-health-check.php
+++ template-blocks/block-web-conversion-health-check.php
@@ -18,20 +18,20 @@ if ( $background['image'] )
 <div class="container">
 <?php endif; ?>
 
-<div class="<?php echo $animation; ?> block block--text <?php the_sub_field('classes'); ?> <?php echo $customClass; ?>" style="background-color: <?php echo $background['colour']; ?>; background-image: url('<?php echo $background_url; ?>'); background-position: <?php echo $background['position']; ?>; background-repeat: <?php echo $background['repeat']; ?>; background-size: <?php echo $background['size']; ?>; margin: <?php echo $margin['desktop']; ?>; padding: <?php echo $padding['desktop']; ?>;" data-<?php the_sub_field('layout'); ?>="true">
+<div class="<?php echo $animation; ?> block block--text <?php the_sub_field('classes'); ?> <?php echo $customClass; ?>" style="background-color: rgba(255, 82, 84, 1) !important;; background-image: url('<?php echo $background_url; ?>'); background-position: <?php echo $background['position']; ?>; background-repeat: <?php echo $background['repeat']; ?>; background-size: <?php echo $background['size']; ?>; margin: <?php echo $margin['desktop']; ?>; padding: <?php echo $padding['desktop']; ?>;" data-<?php the_sub_field('layout'); ?>="true">
 
 <?php if($layout == 'full-width'): ?>
     <div class="container">
 <?php endif; ?>
 
-    <div class="block__web_conversion">
-        <section class="section-letstalk section-orange section-letstalk-new">
-            <div class="container-fluid">
-                <p class="mb-3 p-tag-h2-style"><strong>Get your free Website & Converstion Health Check </br> with an experienced digital strategist valued at </br>$2,000HKD.</strong></p>
+    <div class="block__web_conversion" style="padding: 4rem 0 !important;">
+        <div class="section-letstalk section-orange section-letstalk-new">
+            <div class="container text-center text-white">
+                <p class="mb-3 p-tag-h2-style"><strong>Get your free Website & Converstion Health Check </br class="d-none d-lg-block"> with an experienced digital strategist valued at </br class="d-none d-lg-block">>$2,000HKD.</strong></p>
                 <p class="font-italic mb-4" style="font-size: 18px; font-weight:normal;">Hurry! Limited spots available.</p>
                 <a href="/free-growth-strategy" class="btn fp-btn fp-btn-white fp-btn-shadow fp-session free-session-btn">Get my free health check</a>
             </div>
-        </section>
+        </div>
 
 
     </div>
